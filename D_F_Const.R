9f47208ece18bbb565902ece5482cb0f69b22900
Lee Kennedy-Shaffer <lkennedyshaffer@vassar.edu>
2024-04-16 17:39:16 -0400

    RS: Started E[D] functionality

--- D_F_Const.R
+++ D_F_Const.R
@@ -7,6 +7,7 @@
 
 ## Load packages
 require(dplyr)
+require(tibble)
 
 ## Create a data frame where each row corresponds to an entry in vector D
 ### The columns give i, i', j, and j' corresponding to that D
@@ -37,8 +38,87 @@ gen_D <- function(N,J) {
   }
 }
 
-## Take a vector of clusters and a vector of their first periods on intervention,
-### Plus output from gen_D and return
+## Take a vector of unique clusters and a corresponding vector of their first periods on intervention,
+### Plus output from gen_D and return types for E[D]
+### Note that Clusters and StartPeriods can be labelled however desired
+### If list of periods is anything other than 1:J, 
+#### a vector giving the ordered period names should be included as PeriodOrder
+### Otherwise J should indicate the largest period
+gen_ED <- function(Clusters,StartPeriods,J=NULL,PeriodOrder=NULL) {
+  N <- length(Clusters)
+  if (N < 2) {
+    stop(simpleError("There must be at least 2 clusters."))
+  } else if (N != length(StartPeriods)) {
+    stop(simpleError("The length of Clusters and StartPeriods must match."))
+  }
+  
+  if (!is.null(PeriodOrder)) {
+    if (length(unique(PeriodOrder) != length(PeriodOrder))) {
+      warning(simpleWarning("There are repeated names in PeriodOrder. The first occurrence only will be used."))
+      PeriodOrder <- unique(PeriodOrder)
+    }
+    if (!is.null(J)) {
+      if (J != length(PeriodOrder)) {
+        warning(simpleWarning("J is not the total number of periods, ignoring J"))
+        J <- length(PeriodOrder)
+      }
+    } else {
+      J <- length(PeriodOrder)
+    }
+    OrderedPds <- tibble(Labels=PeriodOrder, Periods=1:J)
+    Start_js <- tibble(Clusters=Clusters,Labels=StartPeriods) %>% 
+      left_join(OrderedPds,by="Labels") %>% 
+      dplyr::rename(Start_j=Periods) %>%
+      dplyr::select(c(Clusters,Start_j)) %>%
+      dplyr::arrange(Start_j) %>%
+      mutate(Cl.Num=1:N)
+  } else if (is.null(J)) {
+    stop(simpleError("Must specify either the vector of ordered period names PeriodOrder or the total number of periods J"))
+  } else if (!is.numeric(J)) {
+    stop(simpleError("J must be numeric"))
+  } else if (J < 2) {
+    stop(simpleError("J must be at least 2."))
+  } else {
+    if (abs(J-round(J)) > .Machine$double.eps^2) {
+      J <- round(J)
+      warning(simpleWarning(paste0("J must be an integer. Using nearest integer: ",J)))
+    }
+    if (!is.numeric(StartPeriods)) {
+      stop(simpleError("Must either specify numeric StartPeriods or ordered PeriodOrder."))
+    } else if (min(StartPeriods) < 1 | max(StartPeriods) > J) {
+      stop(simpleError("All StartPeriods must be between 1 and J, inclusive."))
+    } else {
+      Start_js <- tibble(Clusters=Clusters,Start_j=StartPeriods) %>%
+        dplyr::arrange(Start_j) %>%
+        mutate(Cl.Num=1:N)
+    }
+  }
+  
+  D <- gen_D(N,J) %>% 
+    left_join(Start_js %>% dplyr::select(!(Clusters)) %>% 
+                dplyr::rename(i=Cl.Num,Start.i=Start_j), by="i") %>%
+    left_join(Start_js %>% dplyr::select(!(Clusters)) %>% 
+                dplyr::rename(i.prime=Cl.Num,Start.i.prime=Start_j), by="i.prime") %>%
+    mutate(i.j.leadlag=j-Start.i+1,
+           ip.j.leadlag=j-Start.i.prime+1,
+           i.jp.leadlag=j.prime-Start.i+1,
+           ip.jp.leadlag=j.prime-Start.i.prime+1,
+           i.type=if_else(i.j.leadlag > 0,"Always-Treated",
+                          if_else(i.jp.leadlag <= 0, "Always-Control","Switch")),
+           ip.type=if_else(ip.j.leadlag > 0,"Always-Treated",
+                          if_else(ip.jp.leadlag <= 0, "Always-Control","Switch")),
+           Type=factor(if_else(i.type=="Always-Control",1,
+                        if_else(i.type=="Switch",
+                                if_else(ip.type=="Switch",4,2),
+                                if_else(ip.type=="Always-Control",3,
+                                        if_else(ip.type=="Switch",5,6)))),
+           levels=1:6, labels=c("Both Always-Control",
+                                "Switch vs. Always-Control",
+                                "Always-Treated vs. Always-Control",
+                                "Both Switch",
+                                "Always-Treated vs. Switch",
+                                "Both Always-Treated")))
+}
 
 
 
