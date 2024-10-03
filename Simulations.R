38124dc0224aade057fa21b397cb6122572eded5
Lee Kennedy-Shaffer <lee.kennedy-shaffer@yale.edu>
2024-10-03 16:25:52 -0400

    RS: set up for sim run 3

--- Simulations.R
+++ Simulations.R
@@ -25,7 +25,7 @@ Sim_Frame <- function(N, J, StartingPds=NULL) {
       warning(simpleWarning(message="Assuming one switch per period starting in period 1"))
       StartingPds <- 1:J
     } else if (N %% (J-1) == 0) {
-      warning(simpleWarning(message=paste0("Assuming ",N/(J-1), " switches per period starting in period 1.")))
+      warning(simpleWarning(message=paste0("Assuming ",N/(J-1), " switches per period starting in period 2.")))
       StartingPds <- rep(2:J, each=N/(J-1))
     } else if (N %% J == 0) {
       warning(simpleWarning(message=paste0("Assuming ",N/J, " switches per period starting in period 1.")))
@@ -130,11 +130,12 @@ Sim_Analyze <- function(Sim.Dat,
                         CPI=FALSE,
                         CPI.T=FALSE,
                         CPI.D=FALSE,
+                        CPI.DT=FALSE,
                         GEE=FALSE, #Note: GEE comp is very slow; ~1min/GEE fit
                         corstr="exchangeable") {
   Obs.mat<- t(Sim.Dat[,"Y.ij.bar",,drop=TRUE])
   Results <- Obs.mat %*% as.matrix(Sim.Wt)
-  if (MEM | CPI | CPI.T | CPI.D | GEE) {
+  if (MEM | CPI | CPI.T | CPI.D | CPI.DT | GEE) {
     Sim.Dat.long <- apply(Sim.Dat, 3, 
                           FUN=function(x) as_tibble(x) %>% 
                             dplyr::select(-c("Y.ij.bar","Y.ij.sd")) %>%
@@ -161,7 +162,7 @@ Sim_Analyze <- function(Sim.Dat,
         dplyr::select(starts_with("Interv")) %>%
         dplyr::mutate(across(.cols=-c("Interv"),
                              .fns=~.x+Interv)) %>%
-        dplyr::rename(`Interv:factor(Period)0`=Interv)
+        dplyr::rename(`Interv:factor(Period)7`=Interv)
       colnames(CPI.T_Res) <- gsub(".*)","CPI.T_",colnames(CPI.T_Res))
       CPI.T_Res$CPI.T_AvgExLast <- apply(CPI.T_Res, 1, mean, na.rm=TRUE)
       CPI.T_Res$CPI.T_Middle <- apply(CPI.T_Res[,paste0("CPI.T_",2:4)], 1, mean, na.rm=TRUE)
@@ -176,11 +177,41 @@ Sim_Analyze <- function(Sim.Dat,
         dplyr::select(starts_with("Interv")) %>%
         dplyr::mutate(across(.cols=-c("Interv"),
                              .fns=~.x+Interv)) %>%
-        dplyr::rename(`Interv:factor(Diff)0`=Interv)
+        dplyr::rename(`Interv:factor(Diff)7`=Interv)
       colnames(CPI.D_Res) <- gsub(".*)","CPI.D_",colnames(CPI.D_Res))
       CPI.D_Res$CPI.D_Avg <- apply(CPI.D_Res, 1, mean, na.rm=TRUE)
       CPI.D_Res$CPI.D_Middle <- apply(CPI.D_Res[,paste0("CPI.D_",2:4)], 1, mean, na.rm=TRUE)
       Results <- cbind(Results, as.matrix(CPI.D_Res))
+    }
+    if (CPI.DT) {
+      CPI.DT_Res <- as_tibble(t(sapply(Sim.Dat.long,
+                                      FUN=function(x) fixef(lmer(Y~Interv+Interv:factor(Diff):factor(Period) + 
+                                                                   factor(Period)+ (1|Cluster) + (1|CPI),
+                                                                 data=x %>% dplyr::mutate(CPI=paste(Cluster,Period,sep="_"),
+                                                                                          Diff=if_else(Period-Start < 0,0,Period-Start+1)),
+                                                                 control=lmerControl(check.rankX="silent.drop.cols")))))) %>%
+        dplyr::select(starts_with("Interv")) %>%
+        dplyr::mutate(across(.cols=-c("Interv"),
+                             .fns=~.x+Interv)) %>%
+        dplyr::rename(`Interv:factor(Diff)6:factor(Period)7`=Interv)
+      colnames(CPI.DT_Res) <- paste0("CPI.DT_a",substr(colnames(CPI.DT_Res),20,20),
+                                     "_j",substr(colnames(CPI.DT_Res),36,36))
+      CPI.DT_Res_orig <- CPI.DT_Res
+      CPI.DT_Res$CPI.DT_Avg <- apply(CPI.DT_Res_orig, 1, mean, na.rm=TRUE)
+      CPI.DT_Res$CPI.DT_AvgEx8 <- apply(CPI.DT_Res_orig %>% dplyr::select(-ends_with("j8")), 1, mean, na.rm=TRUE)
+      for (i in 1:6) {
+        CPI.DT_Res[,paste0("CPI.DT_D",i)] <- apply(CPI.DT_Res_orig %>% dplyr::select(contains(paste0("a",i))), 
+                                                   1, mean, na.rm=TRUE)
+      }
+      for (i in 2:7) {
+        CPI.DT_Res[,paste0("CPI.DT_T",i)] <- apply(CPI.DT_Res_orig %>% dplyr::select(contains(paste0("j",i))), 
+                                                   1, mean, na.rm=TRUE)
+      }
+      CPI.DT_Res$CPI.DT_DAvg <- apply(CPI.DT_Res %>% dplyr::select(starts_with("CPI.DT_D")),
+                                      1, mean, na.rm=TRUE)
+      CPI.DT_Res$CPI.DT_TAvg <- apply(CPI.DT_Res %>% dplyr::select(starts_with("CPI.DT_T")),
+                                      1, mean, na.rm=TRUE)
+      Results <- cbind(Results, as.matrix(CPI.DT_Res))
     }
     if (GEE) {
       GEE_Res <- sapply(Sim.Dat.long,
@@ -214,6 +245,7 @@ Sim_Permutation <- function(Sim.Dat,
                         CPI=FALSE,
                         CPI.T=FALSE,
                         CPI.D=FALSE,
+                        CPI.DT=FALSE,
                         GEE=FALSE, #Note: GEE comp is very slow
                         corstr="exchangeable") {
   if (is.null(N) | is.null(J)) {
@@ -269,6 +301,36 @@ Sim_Permutation <- function(Sim.Dat,
       CPI.D_Res$CPI.D_Avg <- apply(CPI.D_Res, 1, mean, na.rm=TRUE)
       CPI.D_Res$CPI.D_Middle <- apply(CPI.D_Res[,paste0("CPI.D_",2:4)], 1, mean, na.rm=TRUE)
       Res.perm <- cbind(Res.perm, as.matrix(CPI.D_Res))
+    }
+    if (CPI.DT) {
+      CPI.DT_Res <- as_tibble(t(sapply(Sim.Dat.long,
+                                       FUN=function(x) fixef(lmer(Y~Interv+Interv:factor(Diff):factor(Period) + 
+                                                                    factor(Period)+ (1|Cluster) + (1|CPI),
+                                                                  data=x %>% dplyr::mutate(CPI=paste(Cluster,Period,sep="_"),
+                                                                                           Diff=if_else(Period-Start < 0,0,Period-Start+1)),
+                                                                  control=lmerControl(check.rankX="silent.drop.cols")))))) %>%
+        dplyr::select(starts_with("Interv")) %>%
+        dplyr::mutate(across(.cols=-c("Interv"),
+                             .fns=~.x+Interv)) %>%
+        dplyr::rename(`Interv:factor(Diff)6:factor(Period)7`=Interv)
+      colnames(CPI.DT_Res) <- paste0("CPI.DT_a",substr(colnames(CPI.DT_Res),20,20),
+                                     "_j",substr(colnames(CPI.DT_Res),36,36))
+      CPI.DT_Res_orig <- CPI.DT_Res
+      CPI.DT_Res$CPI.DT_Avg <- apply(CPI.DT_Res_orig, 1, mean, na.rm=TRUE)
+      CPI.DT_Res$CPI.DT_AvgEx8 <- apply(CPI.DT_Res_orig %>% dplyr::select(-ends_with("j8")), 1, mean, na.rm=TRUE)
+      for (i in 1:6) {
+        CPI.DT_Res[,paste0("CPI.DT_D",i)] <- apply(CPI.DT_Res_orig %>% dplyr::select(contains(paste0("a",i))), 
+                                                   1, mean, na.rm=TRUE)
+      }
+      for (i in 2:7) {
+        CPI.DT_Res[,paste0("CPI.DT_T",i)] <- apply(CPI.DT_Res_orig %>% dplyr::select(contains(paste0("j",i))), 
+                                                   1, mean, na.rm=TRUE)
+      }
+      CPI.DT_Res$CPI.DT_DAvg <- apply(CPI.DT_Res %>% dplyr::select(starts_with("CPI.DT_D")),
+                                      1, mean, na.rm=TRUE)
+      CPI.DT_Res$CPI.DT_TAvg <- apply(CPI.DT_Res %>% dplyr::select(starts_with("CPI.DT_T")),
+                                      1, mean, na.rm=TRUE)
+      Res.perm <- cbind(Res.perm, as.matrix(CPI.DT_Res))
     }
     if (GEE) {
       GEE_Res <- sapply(Perm.Dat.Long,
@@ -484,3 +546,104 @@ save(Full_Sim_Res,
      file="sim_res/Full_Sim_Res.Rda")
 write_csv(x=Full_Sim_Res,
           file="sim_res/Full_Sim_Res.csv")
+
+
+
+#### New simulations to run (post-10/3):
+### Parameters for Simulation:
+NumSims.all <- 500
+NumPerms.all <- 250
+Param_Set <- tribble(
+  ~SimNo, ~NumSims, ~NumPerms, ~mu, ~ProbT1, ~sig_nu, ~sig_e, ~m, ~J, ~N,
+  1, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14,
+  2, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14,
+  3, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14,
+  4, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14,
+  5, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14,
+  6, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14,
+  7, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14,
+  8, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14,
+  9, NumSims.all, NumPerms.all, 0.3, 1, 0.01, 0.1, 100, 8, 14
+)
+
+Theta_Set <- list(list(Type=5, ThetaDF=tibble(Theta=0), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.T","CPI.D"), corstr=NULL),
+                  list(Type=5, ThetaDF=tibble(Theta=-0.02), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.T","CPI.D"), corstr=NULL),
+                  list(Type=5, ThetaDF=tibble(Theta=-0.04), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.T","CPI.D"), corstr=NULL),
+                  list(Type=4, ThetaDF=tibble(j=2:8, Theta=seq(from=-0.07,to=0.05,by=0.02)), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.T"), corstr=NULL),
+                  list(Type=4, ThetaDF=tibble(j=2:8, Theta=c(-0.07,-0.06,-0.04,0,0.03,0.02,0.01)), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.T"), corstr=NULL),
+                  list(Type=4, ThetaDF=tibble(j=2:8, Theta=c(rep(-0.03,4),rep(0,3))), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.T"), corstr=NULL),
+                  list(Type=3, ThetaDF=tibble(a=1:7, Theta=seq(from=-0.01,to=-0.04,by=-0.005)), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.D"), corstr=NULL),
+                  list(Type=3, ThetaDF=tibble(a=1:7, Theta=c(rep(0,2),rep(-0.03,5))), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.D"), corstr=NULL),
+                  list(Type=3, ThetaDF=tibble(a=1:7, Theta=seq(from=-0.07,to=0.05,by=0.02)), Comps=c("TW","CS","SA","CH","CO","NP","CPI","CPI.D"), corstr=NULL))
+
+Alpha1 <- c(-0.007, 0.003, 0.008, -0.016, -0.003, -0.005, -0.012, 
+            0.002, 0.005, -0.001, 0.020, 0, 0.017, -0.011)
+T1 <- c(0,0.08,0.18,0.29,0.30,0.27,0.20,0.13)
+T2 <- c(0,0.02,0.03,0.07,0.13,0.19,0.27,0.30)
+
+### Run Simulations:
+
+load("int/xpert-mv-a_2_CS_0_003.Rda")
+load("int/xpert-mv-a_3_CS_0_003.Rda")
+load("int/xpert-mv-a_4_CS_0_003.Rda")
+load("int/xpert-mv-a_5_CS_0_003.Rda")
+load("int/xpert-mv-a_2_CS_0_333.Rda")
+load("int/xpert-mv-a_3_CS_0_333.Rda")
+load("int/xpert-mv-a_4_CS_0_333.Rda")
+load("int/xpert-mv-a_5_CS_0_333.Rda")
+load("int/xpert-mv-a_2_Ind.Rda")
+load("int/xpert-mv-a_3_Ind.Rda")
+load("int/xpert-mv-a_4_Ind.Rda")
+load("int/xpert-mv-a_5_Ind.Rda")
+load("../int_large/xpert-solve-a_2.Rda")
+load("../int_large/xpert-solve-a_3.Rda")
+load("../int_large/xpert-solve-a_4.Rda")
+load("../int_large/xpert-solve-a_5.Rda")
+## Round 1 seed:
+set.seed(801611)
+
+system.time(simulate_FromSet(Param_Set,
+                             Theta_Set,
+                             StartingPds=NULL,
+                             Alpha1, 
+                             T1, T2, 
+                             MVO_list=list(A2_003=MVOut_2_CS_0_003,
+                                           A3_003=MVOut_3_CS_0_003,
+                                           A4_003=MVOut_4_CS_0_003,
+                                           A5_003=MVOut_5_CS_0_003,
+                                           A2_333=MVOut_2_CS_0_333,
+                                           A3_333=MVOut_3_CS_0_333,
+                                           A4_333=MVOut_4_CS_0_333,
+                                           A5_333=MVOut_5_CS_0_333,
+                                           A2_Ind=MVOut_2_Ind,
+                                           A3_Ind=MVOut_3_Ind,
+                                           A4_Ind=MVOut_4_Ind,
+                                           A5_Ind=MVOut_5_Ind),
+                             SO_list=list(Comp=SolveOut_5),
+                             outdir="sim_res_3",
+                             outname="Sim_Set"))
+
+
+
+### Get simulation results:
+# Full_Sim_Res <- NULL
+# for (i in Param_Set$SimNo) {
+#   load(paste0("sim_res/Sim_Set_",i,".Rda"))
+#   assign(x="Res1", value=get(paste0("Res_Sim_",i)))
+#   load(paste0("sim_res_2/Sim_Set_",i,".Rda"))
+#   assign(x="Res2", value=get(paste0("Res_Sim_",i)))
+#   Ests <- rbind(Res1$Estimates, Res2$Estimates)
+#   PVs <- rbind(Res1$PValues, Res2$PValues)
+#   Res_int <- tibble(SimNo=rep(i,4), 
+#                     Result=c("Mean Estimate","Median Estimate","SD Estimate","Power"))
+#   Full_Sim_Res <- Full_Sim_Res %>% bind_rows(cbind(Res_int, rbind(apply(Ests, 2, mean, na.rm=TRUE),
+#                                                                   apply(Ests, 2, median, na.rm=TRUE),
+#                                                                   apply(Ests, 2, sd, na.rm=TRUE),
+#                                                                   apply(PVs, 2, function(x) mean(x <= 0.05, na.rm=TRUE)))))
+#   rm(list=c("Res1","Res2","Ests","PVs",
+#             paste0("Res_Sim_",i),"Res_int"))
+# }
+# save(Full_Sim_Res,
+#      file="sim_res/Full_Sim_Res.Rda")
+# write_csv(x=Full_Sim_Res,
+#           file="sim_res/Full_Sim_Res.csv")
