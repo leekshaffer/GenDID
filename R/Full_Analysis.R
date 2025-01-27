f58aa231f35e5a01c4ff7c61b44d6e365e8249d9
howardbaik <howardbaik43@gmail.com>
2025-01-27 14:20:10 -0500

    Document exported functions

--- R/Full_Analysis.R
+++ R/Full_Analysis.R
@@ -5,25 +5,59 @@
 ###### Updated 2024/08/12 #############
 #######################################
 
-source("R/D_F_Const.R")
-source("R/Rank_Analysis.R")
-source("R/Var_Min.R")
-source("R/Solver.R")
+# source("R/D_F_Const.R")
+# source("R/Rank_Analysis.R")
+# source("R/Var_Min.R")
+# source("R/Solver.R")
 
-## Inputs to Solve_Assumption function:
-### Amat is the A matrix for the setting
-### StartTimes is a data frame/tibble with two columns that will be input to gen_DFT:
-##### Cluster has the cluster names in order by starting time
-##### StartPd has the starting times for those clusters
-### OrderedPds is the vector of labels for the periods that will be observed, in temporal order
-### Assumption is the numbered assumption setting for the treatment effects (1--5)
-### v.Mat is the v vector of the desired estimand, or a matrix of such column vectors
-### save_loc is the folder to save it in (be sure to end with / if anything other than blank is passed)
-### save_prefix is the prefix to put on the save files
-
-## Function to run solver for any assumption and v matrix
-Solve_Assumption <- function(Amat,StartTimes,OrderedPds,
-                             Assumption,v.Mat,
+#' Solve Assumption Function
+#'
+#' This function runs a solver for any specified assumption and v matrix. It generates
+#' the necessary D, F, and Theta matrices and solves for weights based on the provided
+#' inputs, saving the output to a specified location.
+#'
+#' @param Amat A matrix (\eqn{A}) used in the setting. Typically the design or constraint matrix.
+#' @param StartTimes A data frame or tibble with two columns:
+#'   \describe{
+#'     \item{Cluster}{Cluster names in order by starting time.}
+#'     \item{StartPd}{Starting periods for the corresponding clusters.}
+#'   }
+#' @param OrderedPds A vector of labels for the periods to be observed, in temporal order.
+#' @param Assumption A numeric value (1 to 5) representing the assumption setting for the treatment effects.
+#' @param v.Mat A vector (\eqn{v}) of the desired estimand or a matrix of such column vectors.
+#' @param save_loc A string specifying the folder to save the results. Ensure to end the string with a `/` if a directory is provided. Defaults to an empty string, meaning the current working directory.
+#' @param save_prefix A string prefix to prepend to the saved file names. Defaults to `"solve-a_"`.
+#'
+#' @return A list containing:
+#'   \describe{
+#'     \item{Amat}{The input \eqn{A} matrix.}
+#'     \item{DFT}{The generated D, F, and Theta matrices (from `gen_DFT`).}
+#'     \item{Solve}{The output of the solver (from `solve_WA`).}
+#'   }
+#'   Additionally, this function saves the result as an `.Rda` file to the specified location.
+#'
+#' @details
+#' This function handles generating matrices (`D`, `F`, `Theta`) and solving for weights under
+#' the specified assumptions. The results are saved in a file named using the provided assumption
+#' and the `save_prefix`.
+#'
+#' @examples
+#' # Example usage:
+#' Amat <- matrix(c(1, 0, 0, 1), nrow = 2)
+#' StartTimes <- data.frame(Cluster = c("A", "B"), StartPd = c(1, 2))
+#' OrderedPds <- c(1, 2, 3, 4)
+#' Assumption <- 1
+#' v.Mat <- matrix(c(1, 0, 0, 1), nrow = 2)
+#' save_loc <- "results/"
+#' save_prefix <- "solve-a_"
+#' result <- Solve_Assumption(Amat, StartTimes, OrderedPds, Assumption, v.Mat, save_loc, save_prefix)
+#'
+#' @export
+Solve_Assumption <- function(Amat,
+                             StartTimes,
+                             OrderedPds,
+                             Assumption,
+                             v.Mat,
                              save_loc="",
                              save_prefix="solve-a_") {
   ## Generate D,F,Theta matrices:
@@ -42,10 +76,16 @@ Solve_Assumption <- function(Amat,StartTimes,OrderedPds,
          value=list(Amat=Amat,
                     DFT=DFT_int,
                     Solve=solve_int))
+
+  # TODO: Inform user that you are saving variable
+  # to a .Rda file using the specified save_loc and save_prefix.
   save(list=paste0("SolveOut_",Assumption),
        file=paste0(save_loc,save_prefix,Assumption,".Rda"))
   return(get(paste0("SolveOut_",Assumption)))
 }
+
+
+
 
 ## Permutation Inference
 ## StartTimes is a data frame with a Cluster column and StartPd column
@@ -57,6 +97,8 @@ Permute_data <- function(StartTimes, J, data, Obs.weights) {
   return(t(Obs.weights) %*% as.matrix(cbind(ST_P, data) %>% dplyr::arrange(StartPd,Cluster) %>%
                                         dplyr::select(-c(Cluster,StartPd))))
 }
+
+
 ## Observations is a vector with the observed outcomes, ordered by cluster then period
 ## N is the total number of clusters/units
 ## J is the total number of periods (Note: N*J should be the number of rows in Observations)
@@ -143,6 +185,8 @@ MV_Assumption <- function(SolveOut,
            value=list(Amat=SolveOut$Amat,
                       D_Full=D_Full,
                       MV=MV_int))
+    # TODO: Inform user that you are saving variable
+    # to a .Rda file using the specified save_loc and save_prefix.
     save(list=paste0("MV_",Assumption,"_",SigmaName),
          file=paste0(save_loc,save_prefix,Assumption,"_",SigmaName,".Rda"))
     return(get(paste0("MV_",Assumption,"_",SigmaName)))
@@ -168,11 +212,10 @@ MV_Assumption <- function(SolveOut,
                       D_Full=D_Full,
                       MV=MV_int,
                       P_Values=PVals))
+    # TODO: Inform user that you are saving variable
+    # to a .Rda file using the specified save_loc and save_prefix.
     save(list=paste0("MVOut_",Assumption,"_",SigmaName),
          file=paste0(save_loc,save_prefix,Assumption,"_",SigmaName,".Rda"))
     return(get(paste0("MVOut_",Assumption,"_",SigmaName)))
   }
 }
-
-
-
